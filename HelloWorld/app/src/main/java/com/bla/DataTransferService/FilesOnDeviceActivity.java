ecfbefbfdbff78f26e2966fdf4e93386cf51d298
Thomas <Thomas>
2015-12-12 21:58:44 +0100

    advanced listview for files

--- HelloWorld/app/src/main/java/com/bla/DataTransferService/FilesOnDeviceActivity.java
+++ HelloWorld/app/src/main/java/com/bla/DataTransferService/FilesOnDeviceActivity.java
@@ -1,22 +1,49 @@
 package com.bla.DataTransferService;
 
-import android.app.LoaderManager;
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
+import android.content.Context;
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.support.annotation.IdRes;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
 import android.widget.ListView;
-import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class FilesOnDeviceActivity extends AppCompatActivity{
+
+public class FilesOnDeviceActivity extends AppCompatActivity {
+
+
+    public class MyArrayAdapter extends ArrayAdapter<String> {
+        public View view;
+        private List<String> files;
+
+        public MyArrayAdapter(Context context, @LayoutRes int resource, @IdRes int textViewResourceId, @NonNull List<String> objects) {
+            super(context, resource,textViewResourceId, objects);
+            this.files = objects;
+        }
+
+        @Override
+        public View getView(int pos, View convertView, ViewGroup parent) {
+            this.view = convertView;
+            if (this.view == null) {
+                LayoutInflater vi = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                this.view = vi.inflate(R.layout.file_list_item, null);
+            }
+            TextView textView = (TextView) view.findViewById(R.id.txtFileName);
+            textView.setText(this.files.get(pos));
+            return view;
+        }
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,28 +52,30 @@ public class FilesOnDeviceActivity extends AppCompatActivity{
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-
-        ArrayList valueList = new ArrayList<String>();
-        for (int i = 0; i < 20; i++){
-            valueList.add("value"+i);
-        }
-     //   ListAdapter adapter = new ArrayAdapter(getApplicationContext(), R.layout.file_list_item, valueList);
-
-      //  mAdapter = new SimpleCursorAdapter(this,R.layout.file_list_item, null,fromColumns, toViews, 0);
-
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.file_list_item, R.id.textView1, valueList);
+        FileService fileService = new FileService(this);
+        ArrayList<String> files = fileService.getFilesFromDisk();
+        files.add("some_dummy_file.txt");
 
 
+//        ArrayList valueList = new ArrayList<String>();
+//        for (int i = 0; i < 20; i++) {
+//            valueList.add("value" + i);
+//        }
+        //   ListAdapter adapter = new ArrayAdapter(getApplicationContext(), R.layout.file_list_item, valueList);
+
+        //  mAdapter = new SimpleCursorAdapter(this,R.layout.file_list_item, null,fromColumns, toViews, 0);
+
+       //ArrayAdapter adapter = new ArrayAdapter<String>(this, R.layout.file_list_item, R.id.txtFileName, valueList)
+        MyArrayAdapter adapter = new MyArrayAdapter(this, R.layout.file_list_item, R.id.txtFileName, files);
 
 
 
-        final ListView lv = (ListView)findViewById(R.id.listView);
+        final ListView lv = (ListView) findViewById(R.id.listView);
 
         lv.setAdapter(adapter);
 
 
     }
-
 
 
 }
