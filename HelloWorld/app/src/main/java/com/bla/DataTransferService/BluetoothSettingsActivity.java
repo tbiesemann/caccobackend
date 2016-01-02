2b1382acd018c105d60e15d55a2e8cbc490a7aa1
Thomas Biesemann <thomas.biesemann@sap.com>
2016-01-02 16:26:28 +0100

    Cleanup bluetooth activit

--- HelloWorld/app/src/main/java/com/bla/DataTransferService/BluetoothSettingsActivity.java
+++ HelloWorld/app/src/main/java/com/bla/DataTransferService/BluetoothSettingsActivity.java
@@ -61,64 +61,20 @@ public class BluetoothSettingsActivity extends AppCompatActivity {
             }
         });
 
-//        mReceiver = new BroadcastReceiver() {
-//
-//            @Override
-//            public void onReceive(Context context, Intent intent) {
-//                String action = intent.getAction();
-//                // When discovery finds a device
-//                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-//                    // Get the BluetoothDevice object from the Intent
-//                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-//                    String discoveredDeviceName = device.getName();
-//
-//                    //get device name from textbox
-//                    final EditText inputField = (EditText) findViewById(R.id.txtBluetoothDeviceName);
-//                    String deviceName = inputField.getText().toString();
-//
-//                    if (deviceName.equals(discoveredDeviceName)) {
-//                        log("Device " + discoveredDeviceName + " found");
-//                        setBluetoothDevice(device);
-//                    } else {
-//                        log("Device " + discoveredDeviceName + " found but does not match " + deviceName);
-//                    }
-//                }
-//            }
-//        };
-
-//        // Register the BroadcastReceiver
-//        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-//        registerReceiver(mReceiver, filter);
 
         final EditText inputField = (EditText) findViewById(R.id.txtBluetoothDeviceName);
         this.bluetooth = new BluetoothUtilities(inputField.getText().toString(), this);
     }
 
 
-//    private void setBluetoothDevice(BluetoothDevice device) {
-//        UUID uuid;
-//        try {
-//            uuid = device.getUuids()[0].getUuid();
-//        } catch (NullPointerException e) {
-//            uuid = UUID.fromString("00000000-0000-0000-0000-000000000001"); //default UUID
-//        }
-//        log("Establishing connection with device " + device.getName() + " and address " + device.getAddress() + " and UUID " + uuid.toString());
-//        try {
-//            socket = device.createRfcommSocketToServiceRecord(uuid);
-//            socket.connect();
-//        } catch (IOException e) {
-//            log("Cannot establish connection - error ocurred " + e.toString());
-//            return;
-//        }
-//        this.log("Bluetooth connection is established");
-//
-//    }
 
     public void log(String text) {
         TextView console = (TextView) findViewById(R.id.txtConsole);
         console.setText(console.getText() + "\n" + text);
         System.out.println(text);
     }
+
+
 
 
     private void testConnection() {
@@ -132,40 +88,16 @@ public class BluetoothSettingsActivity extends AppCompatActivity {
         this.bluetooth.enableBluetoothAdapter();
         this.bluetooth.startDeviceDiscovery();
 
-//
-//        this.log("Bluetooth adapter is available");
-//        this.bluetooth
-//        if (!mBluetoothAdapter.isEnabled()) {
-//            this.log("Enabling Bluetooth adapter...");
-//            mBluetoothAdapter.enable();
-//        }
-//        this.log("Bluetooth adapter is enabled");
-//        this.log("Starting device discovery...");
-//        mBluetoothAdapter.startDiscovery();
 
     }
-
-//    private BluetoothSocket socket;
-//    private BroadcastReceiver mReceiver;
+;
 
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         this.bluetooth.destroy();
-//        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-//        if (mBluetoothAdapter != null) {
-//            mBluetoothAdapter.cancelDiscovery();
-//        }
 
-//        if (this.socket != null) {
-//            try {
-//                this.socket.close();
-//            } catch (IOException ex) {
-//            }
-//        }
-//
-//        this.unregisterReceiver(mReceiver);
 
     }
 
