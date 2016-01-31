111f085eec3ec997535d6310cdff7f1dd2af7771
Thomas Biesemann <thomas.biesemann@sap.com>
2016-01-31 13:37:13 +0100

    Write power state to log file

---
+++ HelloWorld/app/src/main/java/com/bla/DataTransferService/PlugInControlReceiver.java
@@ -0,0 +1,20 @@
+package com.bla.DataTransferService;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+
+public class PlugInControlReceiver extends BroadcastReceiver {
+    public void onReceive(Context context , Intent intent) {
+        String action = intent.getAction();
+
+        if(action.equals(Intent.ACTION_POWER_CONNECTED)) {
+            GlobalState.getInstance().log("Warning: power is connected again");
+
+        }
+        else if(action.equals(Intent.ACTION_POWER_DISCONNECTED)) {
+            GlobalState.getInstance().log("Warning: power was disconnected");
+        }
+    }
+}
